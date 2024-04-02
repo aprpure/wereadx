2685aa1351ae96a1c39485c09d0737ec5f0a5cef
merlin <35722236+aprpure@users.noreply.github.com>
2024-04-03 03:29:17 +0800

    Update db.ts

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,16 +1,17 @@
 import { postgres } from "../deps.ts";
 import runtime from "../runtime.ts";
 
-const sql = ''
+let sql = ''; // 将变量声明为let，以便在后面重新赋值
+
 // 检查 runtime.databaseUrl 是否存在
 if (runtime.databaseUrl) {
-  // 如果存在，则使用 postgres 模块构建数据库连接
+    // 如果存在，则使用 postgres 模块构建数据库连接
     sql = postgres.default(runtime.databaseUrl, {
-    onnotice: () => {},
-  });
+        onnotice: () => {},
+    });
 } else {
-  // 如果不存在，则打印一条消息并跳过
-  console.log("Database URL is missing. Skipping database connection setup.");
+    // 如果不存在，则打印一条消息并跳过
+    console.log("Database URL is missing. Skipping database connection setup.");
 }
 
 export default sql;
